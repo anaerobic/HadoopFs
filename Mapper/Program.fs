d83b54fb93f8e9835e7b8e7aadfa77ab53587e69
anaerobic <mwm.cal@gmail.com>
2015-05-12 16:46:04 -0700

    splitting Mapper and Reducer into separate projects

---
+++ Mapper/Program.fs
@@ -0,0 +1,8 @@
+﻿open HadoopFs.Core
+open HadoopFs.IO
+open HadoopFs.Samples
+
+[<EntryPoint>]
+let mainMap argv = 
+    doMap <| ManyOutputs WordCount.Mapper
+    0
---
+++ Reducer/Program.fs
@@ -0,0 +1,8 @@
+﻿open HadoopFs.Core
+open HadoopFs.IO
+open HadoopFs.Samples
+
+[<EntryPoint>]
+let mainReduce argv = 
+    doReduce <| SingleOutput WordCount.Reducer
+    0
