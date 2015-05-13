60fe30ea31c85f8432f501bfea131bcbf48721c2
mwmartin <mwm.cal@gmail.com>
2015-05-13 08:45:05 -0600

    fixing count-words.sh

--- hadoop/share/count-words.sh
+++ hadoop/share/count-words.sh
@@ -8,7 +8,7 @@ bin/hdfs dfs -mkdir /hdfs/words
 
 bin/hdfs dfs -copyFromLocal /share/data/words/* /hdfs/words/
 
-bin/hdfs fs -rm -r /hdfs-output/words
+bin/hdfs dfs -rm -r /hdfs-output/words
 
 bin/hadoop jar /usr/local/hadoop-2.6.0/share/hadoop/tools/lib/hadoop-streaming-2.6.0.jar -input "/hdfs/words" -output "/hdfs-output/words" -mapper "mono /share/Debug/Mapper.exe" -file "/share/Debug/Mapper.exe"
 
